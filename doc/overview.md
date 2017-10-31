3eae06429c3ea781f661c0514d9bb901f5c77428
Ilya Khaprov <dead.trickster@gmail.com>
2017-10-31 15:10:42 +0300

    Bump to v0.1.0

--- doc/overview.md
+++ doc/overview.md
@@ -1,6 +1,6 @@
 @copyright 2017 Ilya Khaprov <<i.khaprov@gmail.com>>.
 @title prometheus_cowboy
-@version 0.0.3
+@version 0.1.0
 
 @doc
 
--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PrometheusCowboy.Mixfile do
 
   def project do
     [app: :prometheus_cowboy,
-     version: "0.0.3",
+     version: "0.1.0",
      deps: deps(),
      description: description(),
      package: package()]
